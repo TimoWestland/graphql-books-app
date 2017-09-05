4b5da61fd1a9feefb54431206a4fde09944f9318
Timo Westland <timo.westland@incentro.com>
2017-09-05 09:59:49 +0200

    Build basic shop

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require('express');
-const products = require('./app/routers/products');
 const app = express();
+const products = require('./app/routers/products');
+const path = require('path');
+const config = require('./config.json');
+const numeral = require('numeral');
+const url = require('url');
 
-app.listen(3000, function() {
-  console.log('api works!');
+app.use((req, res, next) => {
+  res.locals = {
+    snipcartApiKey: config.snipcartApiKey,
+    formatMoney: (number) => {
+      return numeral(number).format('0.00$')
+    },
+    fullUrl: (path) => {
+      return url.format({
+        protocol: req.protocol,
+        host: req.get('host'),
+        pathname: path
+      })
+    }
+  };
+  next()
+});
+
+app.use('/', products);
+
+app.use((req, res, next) => {
+  res.status(404)
 });
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'app/views'));
+app.set('views', path.join(__dirname, '/app/views'));
 
-app.use('/', products);
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Listening on port 3000.');
+});
